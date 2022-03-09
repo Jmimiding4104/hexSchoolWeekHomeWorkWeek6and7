6dc67f69f5191c845dab6ddb8569186a26b96506
Jmimiding4104 <Jmimiding4104@gmail.com>
2022-03-09 15:15:16 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,59 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/',
+    component: () => import('../views/FrontView.vue'),
+    children: [
+      {
+        path: '',
+        name: 'home',
+        component: HomeView
+      },
+      {
+        path: 'Cart',
+        component: () => import('../views/CartView.vue')
+      },
+      {
+        path: 'Products',
+        component: () => import('../views/ProductsView.vue')
+      },
+      {
+        path: 'Product/:id',
+        component: () => import('../views/ProductView.vue')
+      }
+    ]
+  },
+  {
+    path: '/login',
+    component: () => import('../views/LoginView.vue')
+  },
+  {
+    path: '/admin',
+    component: () => import('../views/DashboardView.vue'),
+    children: [
+      {
+        path: 'AdminCouple',
+        component: () => import('../views/AdminCouple.vue')
+      },
+      {
+        path: 'AdminProducts',
+        component: () => import('../views/AdminProducts.vue')
+      }
+    ]
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+  linkActiveClass: 'active'
+})
+
+export default router
---
+++ src/views/AdminCouple.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>Couple</h2>
+</template>
